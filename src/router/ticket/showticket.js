3fa6f503a6735bac920c0156eb5b55c045eb38fd
sidd98 <sidd.sajwan.1@gmail.com>
2020-05-09 13:41:47 -0400

    added new file

--- src/router/ticket/showticket.js
+++ src/router/ticket/showticket.js
@@ -1,9 +1,10 @@
 const { ticketModels, } = require('../../modules/ticket',);
 
-exports.get = async (req,res) => {
-
+exports.get = async (req, res,) => {
     const { ticket_id, } = req.params;
     const ticket = await ticketModels.getTicket(ticket_id,);
-
-    res.render('index', { ticket, },);
+    if (ticket) {
+        res.render('index', { ticket, },);
+    }
+    res.render('notfound',);
 };
